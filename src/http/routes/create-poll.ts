fa1a2082de45f7df6a7c74f7a134be548963db68
Daniel Carvalho <dscarv27@gmail.com>
2024-02-10 20:51:54 -0300

    conectando websocket com redis para ter votacao em tempo real

--- src/http/routes/create-poll.ts
+++ src/http/routes/create-poll.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod'
-import { prisma } from '../../lib/prisma'
 import { FastifyInstance } from 'fastify'
+
+import { prisma } from '../../lib/prisma'
 
 export async function createPoll(app: FastifyInstance) {
   app.post('/polls', async (request, reply) => {
